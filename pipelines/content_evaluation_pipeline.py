e32c529339bae1e7f3d02a4743c7f16caed09fd1
wongzhenhao <zhenhao1141@gmail.com>
2025-12-30 11:30:08 +0800

    [Feature] ContentOnlyEvalutor added

---
+++ pipelines/content_evaluation_pipeline.py
@@ -0,0 +1,54 @@
+import json
+import os
+import itertools
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from typing import Literal
+
+# from operators.general.chunked_generator import ChunkedPromptedGenerator
+from dataflow.operators.core_text import PandasOperator, PromptedGenerator
+from prompts.evaluation import ContentOnlyEvaluationPrompt
+from dataflow.serving.api_google_vertexai_serving import APIGoogleVertexAIServing
+from operators.evaluation.content_evaluation import ContentOnlyEvaluator
+from dataflow.utils.storage import FileStorage
+
+class ContentOnlyEvaluationPipeline():
+    def __init__(self, entry_file_name:str):
+        self.storage = FileStorage(
+            first_entry_file_name=entry_file_name,
+            cache_path="./evaluation_test_output2",
+            cache_type="json",
+        )
+
+        self.llm_serving = APIGoogleVertexAIServing(
+            project=os.getenv("GOOGLE_CLOUD_PROJECT"),
+            location='us-central1',
+            model_name="gemini-2.5-flash",
+            max_workers=10,
+            max_tokens=64000,
+            use_batch=False,
+            batch_wait=False,
+            use_function_call=False,
+            batch_dataset="evaluation_test",
+            csv_filename="evaluation_test_prompt.csv",
+            bq_csv_filename="evaluation_test_prompt.csv",
+        )
+
+        self.evaluation = ContentOnlyEvaluator(
+            llm_serving = self.llm_serving, 
+            prompt_template=ContentOnlyEvaluationPrompt(),
+            json_schema=ContentOnlyEvaluationPrompt().build_json_schema(),
+        )
+        
+    def forward(self):
+        self.evaluation.run(
+            storage = self.storage.step(),
+            input_key = "content",           # 原文列
+            input_evaluation_keys = ["doi","structure_info","material_indexes","computation_detail","computation_indexes","thermal_properties","mechanical_properties","electrical_or_magnetic_properties"], # 待评估的提取字段列
+            output_key = "content_evaluation"
+        )
+
+
+if __name__ == "__main__":
+    model = ContentOnlyEvaluationPipeline(entry_file_name="./data/BenchmarkCompareEvaluationPipeline/content_extraction.json")
+    model.forward()
